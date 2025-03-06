681ac161be1f8efbd797f7425170fc9a34f968ae
Neelabh Vijayvargia <neelabh.vijayvargia@gmail.com>
2025-03-06 14:22:39 -0600

    helpers

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -1,3 +1,27 @@
 package helpers
 
-func EnforceHTTP
+import (
+	"os"
+	"strings"
+)
+
+func EnforceHTTP(url string) string {
+	if url[:4] != "http" {
+		return "http://" + url
+	}
+
+	return url
+}
+
+func RemoveDomainError(url string) bool {
+	if url == os.Getenv("DOMAIN") {
+		return false
+	}
+
+	newURL := strings.Replace(url, "http://", "", 1)
+	newURL = strings.Replace(newURL, "https://", "", 1)
+	newURL = strings.Replace(newURL, "www.", "", 1)
+	newURL = strings.Split(newURL, "/")[0]
+
+	return newURL != os.Getenv("DOMAIN")
+}
